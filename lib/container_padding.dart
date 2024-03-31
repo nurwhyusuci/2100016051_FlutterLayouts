a5300599887eba3e69ccd03d0dfa09b3ffc8f030
nurwhyusuci <nurwahyusucirahayu@gmail.com>
2024-03-31 21:35:52 +0700

    first commit

---
+++ lib/container_padding.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Layouts',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text("Container & Padding"),
+            ),
+            body: Center(
+              child: Container(
+                color: Colors.red,
+                padding: const EdgeInsets.all(30),
+                child: const Text("Ego Abror R"),
+              ),
+            )));
+  }
+}
